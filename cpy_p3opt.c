2ed5328307d3c20f6546f3bc4314b5fda2d1e9b2
zhangdanfeng <zhangdanfeng@cloudwalk.cn>
2019-06-19 14:38:13 +0000

    cpy_lsopt impl
    Signed-off-by: zhangdanfeng <zhangdanfeng@cloudwalk.cn>

--- cpy_p3opt.c
+++ cpy_p3opt.c
@@ -1,8 +1,9 @@
 #include "cpy_opt.h"
-
+#include <stdio.h>
 #ifdef __linux__
 #ifdef DARMV7ACOUNTER
 #include "gettimearmv7a.h"
+#define gettime getclock
 #else
 #include "gettime.h"
 #define gettime gettimeus
@@ -11,15 +12,20 @@
 #error "do not support this arch"
 #endif // __linux__
 
-double u;
+/* Three lines of rumors */
+/* for understanding how the compiler frontend works, to check the assembly code gcc generated */
+/* for understanding how the compiler backend works, to check the binary code gcc generated    */
+/* for understanding how the CPU works, to observe the behavior of the CPU                     */
 
 /* load test (with neon summing to be somewhat fair against unoptimized version */
-/* for understanding how the compiler works, to check the assembly code gcc generated */
 int cpy_lsopt(double *ptr, int stride, int block_size, int it)
 {
-  register size_t n asm("r4");
+  //printf("%p %d %d %d\n", ptr, stride, block_size, it);
+  register double *entry asm("r4");
+  register size_t n asm("r5");
   n = block_size / stride;
-  stride *= sizeof(double);
+  register size_t stride_r asm("r6");
+  stride_r = stride * sizeof(double);
 
   uint32 hi0, lo0, hi1, lo1;
   gettime(&hi0, &lo0);
@@ -28,27 +34,39 @@ int cpy_lsopt(double *ptr, int stride, int block_size, int it)
     int i, j;
     for (i = 0; i < it; i++) {
       for (j = 0; j < stride; j++) {
+        entry = ptr + j;
+  	//printf("%p %d %d %d %d\n", entry, stride_r, n, i, j);
         /* that addition doesn't make much sense, but we don't care about the result... */
         __asm__ __volatile__(
-	  "asrs    %3, r5, #3 \n"
+          "vsub.i64 d8, d8, d8 \n\t"
+          "vsub.i64 d9, d9, d9 \n\t"
+          "vsub.i64 d10, d10, d10 \n\t"
+          "vsub.i64 d11, d11, d12 \n\t"
+          "asrs r7, %2, #3 \n"
 "ls_loop_8: \n\t"
-	  "cmp r5, #1 \n\t"
-          "blt ls_loop_end\n\t" 
-          "vld1.32 {d0}, [%1], %2 \n\t"
-          "vld1.32 {d1}, [%1], %2 \n\t"
-          "vld1.32 {d2}, [%1], %2 \n\t"
-          "vld1.32 {d3}, [%1], %2 \n\t"
-	  "sub r5, r5, #1 \n\t"
-          "vld1.32 {d4}, [%1], %2 \n\t"
-          "vld1.32 {d5}, [%1], %2 \n\t"
-          "vld1.32 {d6}, [%1], %2 \n\t"
-          "vld1.32 {d7}, [%1], %2 \n\t"
-"ls_loop_end: \n\t"
+          "vld1.32 {d0}, [%0], %1 \n\t"
+          "vadd.i64 d8, d8, d0 \n\t"
+          "vld1.32 {d1}, [%0], %1 \n\t"
+          "vadd.i64 d9, d9, d1 \n\t"
+          "vld1.32 {d2}, [%0], %1 \n\t"
+          "vadd.i64 d10, d10, d2 \n\t"
+          "vld1.32 {d3}, [%0], %1 \n\t"
+          "vadd.i64 d11, d11, d3 \n\t"
+          "sub r7, r7, #1 \n\t"
+          "vld1.32 {d4}, [%0], %1 \n\t"
+          "vadd.i64 d8, d8, d4 \n\t"
+          "vld1.32 {d5}, [%0], %1 \n\t"
+          "vadd.i64 d9, d9, d5 \n\t"
+          "vld1.32 {d6}, [%0], %1 \n\t"
+          "vadd.i64 d10, d10, d6 \n\t"
+          "vld1.32 {d7}, [%0], %1 \n\t"
+          "vadd.i64 d11, d11, d7 \n\t"
+          "cmp r7, #1 \n\t"
+          "bge ls_loop_8 \n\t"
         :
-	:"r"(ptr), "r"(stride), "r"(n)
-        :"cc"
+        :"r"(entry), "r"(stride_r), "r"(n)
+        :"cc", "r7", "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11"
         );
-        ptr++;
       }
     }
   }
